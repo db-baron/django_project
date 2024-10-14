ece2408ee1c2fbe7c07e5ae8d2efd9e31309078c
Daniel Baron <Megatron@Daniels-MacBook-Air.local>
2024-10-14 13:48:31 -0700

    add bootstrap

--- blog/views.py
+++ blog/views.py
@@ -12,8 +12,8 @@ posts = [{'author': 'CoreyMS',
 # Create your views here.
 def home(request):
     context = {'posts': posts}
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
  
